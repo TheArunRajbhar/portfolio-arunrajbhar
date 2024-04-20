ba70ef245d439166d1575282cf6f4e6ec8446265
Arun Rajbhar <arunrajbhar@mayhem-studios.com>
2024-04-20 12:49:22 +0530

    fixed error

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
 import React from "react";
 import "./App.css";
 import Main from "./containers/Main";
-import { Helmet } from 'react-helmet';
+import { Helmet } from "react-helmet";
 
 function App() {
-
   return (
-    <div >
-    <Helmet>
-     <title>Arun Rajbhar</title>
-    <meta name="title" content="Arun Rajbhar" />
-    <meta
-      name="description"
-      content="Arun Rajbhar, a dedicated Full Stack Developer in Mumbai, Maharashtra. Specializing in creating dynamic digital experiences. Explore my portfolio for innovative solutions. Let's connect and discuss your next project."
-    />
-    <meta property="og:type" content="website" />
-    <meta name="robots" content="index, follow" />
-    <meta property="og:title" content="Arun Rajbhar" />
-    <meta
-      property="og:image"
-      content="https://portfolio-arunrajbhar.vercel.app/portfolio.jpeg"
-    />
-     <script type="application/ld+json">
-      {
-        "@context": "https://schema.org",
-        "@type": "WebSite",
-        "name": "Arun Rajbhar",
-        "alternateName": ["AR"],
-        "url": "https://portfolio-arunrajbhar.vercel.app/"
-      }
-    </script>
-    </Helmet>
+    <div>
+      <Helmet>
+        <title>Arun Rajbhar</title>
+        <meta name="title" content="Arun Rajbhar" />
+        <meta
+          name="description"
+          content="Arun Rajbhar, a dedicated Full Stack Developer in Mumbai, Maharashtra. Specializing in creating dynamic digital experiences. Explore my portfolio for innovative solutions. Let's connect and discuss your next project."
+        />
+        <meta property="og:type" content="website" />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="Arun Rajbhar" />
+        <meta
+          property="og:image"
+          content="https://portfolio-arunrajbhar.vercel.app/portfolio.jpeg"
+        />
+        <script type="application/ld+json">
+          {`
+            {
+              "@context": "http://schema.org",
+              "@type": "Organization",
+              "name": "Arjio Store",
+              "url": "https://www.arjiostore.com",
+              "logo": "https://www.arjiostore.com/logo.png"
+            }
+          `}
+        </script>
+      </Helmet>
       <Main />
     </div>
   );
