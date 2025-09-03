7c5801a9c31315f940ef251a83a79f1a1ac7097c
Arun Rajbhar <arunrajbhar1998@gmail.com>
2025-09-03 18:28:04 +0000

    Add @vercel/analytics dependency and integrate into App component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Main from "./containers/Main";
 import { Helmet } from "react-helmet";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
